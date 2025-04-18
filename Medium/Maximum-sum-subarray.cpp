74b7cffb08779db73b7312ea67b76a87017d370b
Yuval-Varke <varke.vishu01@gmail.com>
2025-04-18 23:23:17 +0530

    max sum subarray

---
+++ Medium/Maximum-sum-subarray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int maxSum(int arr[],int n){
+    int res = arr[0];
+    int maxending = arr[0];
+    for(int i=1;i<n;i++){
+        maxending = max(maxending+arr[i],arr[i]);
+        res = max(res,maxending);
+    }
+    return res;
+}
+
+
+
+int main(){
+    int arr[] = {2,3,-8,7,-1,2,3};
+    int n = 7;
+    cout<<maxSum(arr,n);
+    return 0;
+}
