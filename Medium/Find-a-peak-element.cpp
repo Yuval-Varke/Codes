f19a51725df174ba8215337b7f0a4bffba85d2bf
Yuval Varke <varke.vishu01@gmail.com>
2025-05-02 19:16:17 +0530

    Add files via upload

---
+++ Medium/Find-a-peak-element.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int getPeak(int arr[],int n){
+    int low = 0;
+    int high = n-1;
+    while(low<=high){
+        int mid = (low+high)/2;
+        if((mid == 0 || arr[mid-1]<= arr[mid]) && (mid==n-1 || arr[mid+1] <= arr[mid])){
+            return arr[mid];
+        }
+        if(mid > 0 && arr[mid-1] >= arr[mid]){
+            high = mid-1;
+        }
+        else{
+            low = mid+1;
+        }
+    }
+    return -1;
+}
+
+
+int main(){
+    int arr[] = {5,20,40,30,20,50,60};
+    int n = 7;
+    cout<<getPeak(arr,n);
+    return 0;
+}
