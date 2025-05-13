7539efd5e53602e83c1fbcd69ec17c0c91b24c8f
Yuval Varke <varke.vishu01@gmail.com>
2025-05-13 11:47:51 +0530

    matrix

---
+++ Easy/Largest-local-in-matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<int>> maxlocal(n-2,vector<int>(n-2));
+        for(int i=0;i<n-2;i++){
+            for(int j=0;j<n-2;j++){
+                int maxval = 0;
+                for(int x=i;x<i+3;x++){
+                    for(int y=j;y<j+3;y++){
+                        maxval = max(grid[x][y],maxval);
+                    }
+                }
+                maxlocal[i][j] = maxval;
+            }
+        }
+        return maxlocal;
+    }
+};
