d9cace864858d123abd7dcc2289f9de6290cf074
Yuval-Varke <varke.vishu01@gmail.com>
2025-10-11 08:41:30 +0530

    bubble

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-void BubbleSort(int arr[],int n){
+void BubbleSort(int arr[],int n){  // O(n^2)
     for(int i=0;i<n-1;i++){
         bool isSwap = false;
         for(int j=0;j<n-i-1;j++){
