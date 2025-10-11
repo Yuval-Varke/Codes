788dc5d2f3bc4ed392f33b4a245c5c09884d0aee
Yuval-Varke <varke.vishu01@gmail.com>
2025-10-11 08:52:12 +0530

    selection

---
+++ Sorting/SelectionSort.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+void SelectionSort(int arr[],int n){  // O(n^2)
+    for(int i=0;i<n-1;i++){
+        int smallestIdx = i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j] < arr[smallestIdx]){
+                smallestIdx = j;
+            }
+        }
+        swap(arr[i],arr[smallestIdx]);
+    }
+}
+
+
+int main(){
+    int n = 5;
+    int arr[] = {4,1,5,2,3}; 
+    SelectionSort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
